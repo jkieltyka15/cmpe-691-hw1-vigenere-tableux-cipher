79ae50f9991a1e9794b17537347d0e00136bef22
jkieltyka15 <jmkielty@mtu.edu>
2023-02-19 16:59:16 -0500

    ref #1 - Added echoing in.txt into out.txt for the testbench.

--- src/vtc_tb.v
+++ src/vtc_tb.v
@@ -1,9 +1,31 @@
 module vtc_tb();
 
-initial
-  begin
-    $display("Hello World!");
-    $finish;
-  end
+    reg[7:0] buffer;
+    reg[0:0] eof;
+    integer in_file;
+    integer out_file;
+
+    initial begin
+        
+        eof = 0;
+        in_file = $fopen("in.txt", "r");
+        out_file = $fopen("out.txt", "w");
+
+        while(!eof) begin
+
+            buffer[7:0] = $fgetc(in_file);
+            if ($feof(in_file)) begin
+                eof = 1;
+            end 
+            else begin
+                $fwrite(out_file, "%c", buffer[7:0]);
+            end
+
+        end
+
+        $fclose(in_file);
+        $fclose(out_file);
+    
+    end
 
 endmodule
