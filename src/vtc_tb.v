0a5a307acda7fe3c68002f51fecd32e9580b7925
jkieltyka15 <jmkielty@mtu.edu>
2023-02-19 21:54:13 -0500

    refs #14 - Testbench can now get the encrypt flag and key from in.txt.

--- src/vtc_tb.v
+++ src/vtc_tb.v
@@ -3,30 +3,47 @@
 module vtc_tb();
 
     reg[7:0] buffer;
-    reg[0:0] eof;
     integer in_file;
     integer out_file;
 
+    reg encrypt_flag;
+    reg[7:0] key[1024:0];
+    integer key_length = 0;
+
     initial begin
         
-        eof = 0;
+        // open in and out text files
         in_file = $fopen("in.txt", "r");
         out_file = $fopen("out.txt", "w");
 
-        while(!eof) begin
+        // determine whether to encrypt or decrypt
+        encrypt_flag = (8'h30 != $fgetc(in_file));
 
+        // get the key
+        buffer[7:0] = $fgetc(in_file);
+        for (integer i = 0; 1024 > i && ! $feof(in_file); i++) begin
+            
             buffer[7:0] = $fgetc(in_file);
             
-            if ($feof(in_file)) begin
-                eof = 1;
-            end 
+            if (8'h0a == buffer[7:0]) begin
+                i = 1024;
+            end
             else begin
-                buffer[7:0] = to_lower(buffer[7:0]);
-                $fwrite(out_file, "%c", buffer[7:0]);
+                key[i] = to_lower(buffer[7:0]);
+                key_length++;
             end
 
         end
+        
+        $display("encrypt: %d", encrypt_flag);
+        $write("key: ");
+        for(integer i = 0; i < key_length; i++) begin
+            $write("%c", key[i]);
+        end
+        $write("\n");
+        $display("key length: %d", key_length);
 
+        // close in and out text files
         $fclose(in_file);
         $fclose(out_file);
     
